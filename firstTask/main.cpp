5704c9f316ae869da6a0e70f957dab160b117501
openDiv <sema.shulgin@gmail.com>
2024-10-20 18:29:00 +0300

    refactoring + cleared user files

---
+++ firstTask/main.cpp
@@ -0,0 +1,31 @@
+#include <QCoreApplication>
+#include <QtDBus/qdbusconnection.h>
+#include <QtDBus/QDBusContext>
+#include <QDebug>
+#include "permissionsbus.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    if (!QDBusConnection::sessionBus().isConnected())
+    {
+        qDebug()<<"Unable to connect to session bus";
+        return 1;
+    }
+
+    if (!QDBusConnection::sessionBus().registerService(SERVICE_NAME))
+    {
+        qDebug() << "Unable to register service";
+        return 1;
+    }
+
+    permissionsBus receiveObj;
+
+    if(!QDBusConnection::sessionBus().registerObject("/", &receiveObj, QDBusConnection::ExportAllSlots)) {
+        receiveObj.sendErrorReply(QDBusError::Failed, "Unable to register object");
+        return 1;
+    }
+
+    return a.exec();
+}
